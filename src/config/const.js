7e9b653e35fe258541fb25fbe3f2d4370865f2df
Neix20 <txen2000@gmail.com>
2023-09-23 21:17:19 +0800

    Updated Server

--- src/config/const.js
+++ src/config/const.js
@@ -1,10 +1,10 @@
 const APP_VERSION = "1.0.0";
 
-// const SERVER_URL = "https://neix-chs.tech/simulation";
-// const LOG_URL = "https://neix-chs.tech/log";
+const SERVER_URL = "https://neix-chs.tech/simulation";
+const LOG_URL = "https://neix-chs.tech/log";
 
-const SERVER_URL = "http://localhost:5002";
-const LOG_URL = "http://localhost:5001";
+// const SERVER_URL = "http://localhost:5002";
+// const LOG_URL = "http://localhost:5001";
 
 export {
     APP_VERSION,
